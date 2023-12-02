2a996fe29f6ad1f5543a895284d1c1a79a5e5968
LorenaHendricks <130079906+LorenaHendricks@users.noreply.github.com>
2023-12-01 21:32:29 -0500

    Update M03CaseStudy.py

--- M03CaseStudy.py
+++ M03CaseStudy.py
@@ -1,27 +1,34 @@
+# super class
 class Vehicle:
     def __init__(self, vehicle_type):
         self.vehicle_type = vehicle_type
 
-class Automobile:
-    def __init__(self, vehicle_type):
-        super().__init__(vehicle_type)
-        self.year = ""
-        self.make = ""
-        self.model = ""
-        self.doors = ""
-        self.roof = ""
 
-    def display_data(self):
-        print("Vehicle type:", self.vehicle_type)
-        print("Year:", self.year)
-        print("Make:", self.make)
-        print("Model:", self.model)
-        print("Number of doors:", self.doors)
-        print("Type of roof:", self.roof)
-        car = Automobile("car")
-        car.year = input("Enter the year: ")
-        car.make = input("Enter the make: ")
-        car.model = input("Enter the model: ")
-        car.doors = input("Enter the number of doors (2 or 4): ")
-        car.roof = input("Enter the type of roof (solid or sun roof): ")
-        car.display_data()
+# child class
+class Automobile(Vehicle):
+    def __init__(self, vehicle_type, year, make, model, doors, roof):
+        # call super class constructor
+        Vehicle.__init__(self, vehicle_type)
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+
+    # method to return data in required format
+    def __str__(self):
+        return "Vehicle type: " + self.vehicle_type + "\nYear: " + self.year + "\nMake: " + self.make + "\nModel: " + self.model + "\nNumber of doors: " + self.doors + "\nType of roof: " + self.roof
+
+
+if __name__ == "__main__":
+    # get year, make, model, doors and roof from user
+    year = input("Enter year: ")
+    make = input("Enter make: ")
+    model = input("Enter model: ")
+    doors = input("Enter number of doors: ")
+    roof = input("Enter type of roof: ")
+    # create an object of Vehicle type as car
+    carObj = Automobile("car", year, make, model, doors, roof)
+    # print details
+    print("Entered details are =>")
+    print(carObj)
